9f7d7a35c413803089e0c98e772cc8e98cc0c187
ghdtjrals3 <ghdtjrals3@gmail.com>
2023-06-05 17:09:23 +0900

    security exam

---
+++ src/main/java/kopo/poly/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package kopo.poly.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
--- src/main/java/kopo/poly/controller/IndexController.java
+++ src/main/java/kopo/poly/controller/IndexController.java
@@ -2,6 +2,7 @@ package kopo.poly.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -11,7 +12,36 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/user")
     public String user() {
+        return "user";
+    }
 
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @ResponseBody
+    @GetMapping("/joinProc")
+    public String joinProc() {
+        return "회원가입 완료!";
     }
 }
